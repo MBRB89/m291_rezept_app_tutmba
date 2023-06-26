441a4e21294b465f23f3dee969e6995f03e0b23c
MBRB89 <micha.barna@sbw-media.ch>
2023-06-26 13:58:42 +0200

    The mofo code

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/recipe/:slug',
+    name: 'Recipe',
+    component: () => import('../views/RecipeView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
